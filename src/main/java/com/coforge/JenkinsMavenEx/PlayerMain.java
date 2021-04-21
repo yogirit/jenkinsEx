e319d5a1e097e4dfe2d92b252f0cc23602c3c4e6
yogirit <yogeshbhargavidea@gmail.com>
2021-04-21 16:00:17 +0530

    Commit Maven

---
+++ src/main/java/com/coforge/JenkinsMavenEx/PlayerMain.java
@@ -0,0 +1,12 @@
+package com.coforge.JenkinsMavenEx;
+
+public class PlayerMain {
+
+	public static void main(String[] args) {
+		Players sachin = new Players(10,"Sachin","Maharashtra"); 
+		System.out.println(sachin.getJerseyNo()+" : "
+		+sachin.getPlayerName()+" : "+sachin.getState());
+	
+	}
+
+}
---
+++ src/main/java/com/coforge/JenkinsMavenEx/Players.java
@@ -0,0 +1,48 @@
+package com.coforge.JenkinsMavenEx;
+
+public class Players {
+	private int jerseyNo;
+	private String playerName;
+	private String state;
+	
+	public Players() {
+		super();
+	}
+
+	public Players(int jerseyNo, String playerName, String state) {
+		super();
+		this.jerseyNo = jerseyNo;
+		this.playerName = playerName;
+		this.state = state;
+	}
+
+	public int getJerseyNo() {
+		return jerseyNo;
+	}
+
+	public void setJerseyNo(int jerseyNo) {
+		this.jerseyNo = jerseyNo;
+	}
+
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	@Override
+	public String toString() {
+		return "Players [jerseyNo=" + jerseyNo + ", playerName=" + playerName + ", state=" + state + "]";
+	}
+	
+}
